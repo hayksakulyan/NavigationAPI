481f46ad0e01574ca10487439957f20509abd599
hayksakulyan <sakulyanhayk@gmail.com>
2023-07-04 13:31:21 +0400

    "Added project files"

---
+++ NavigationAPI/NavigationAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAPIApp.swift
+//  NavigationAPI
+//
+//  Created by Hayk Sakulyan on 04.07.23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
